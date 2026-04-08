ea22d293079d12c3cc714f5dd6d5ed56f1a94e18
ileniaperrella <ilenia.perrella@pal-robotics.com>
2026-04-08 11:01:05 +0200

    use same wrist model for teleop arms

--- tiago_pro_controller_configuration/launch/default_controllers.launch.py
+++ tiago_pro_controller_configuration/launch/default_controllers.launch.py
@@ -125,13 +125,13 @@ def declare_actions(launch_description: LaunchDescription, launch_args: LaunchAr
 
 
 def configure_side_controllers(context, end_effector_side='right', *args, **kwargs):
-    wrist_model = read_launch_argument(f'wrist_model_{end_effector_side}', context)
-
-    is_teleop = end_effector_side.startswith('teleop')
-
-    if is_teleop:
+    has_teleop_arms = read_launch_argument('has_teleop_arms', context)
+    
+    if has_teleop_arms:
+        wrist_model = read_launch_argument(f'wrist_model_{end_effector_side.replace("teleop_", "")}', context)
         root_link_str = "pilot_station_base_link"
     else:
+        wrist_model = read_launch_argument(f'wrist_model_{end_effector_side}', context)
         root_link_str = "torso_lift_link"
 
     end_effector_arg_name = concatenate_strings(
