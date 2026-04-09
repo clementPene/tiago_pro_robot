6cf5d1a8f1410ae454adfca8ac9ea42c5c4038bc
thomaspeyrucain <thomas.peyrucain@pal-robotics.com>
2026-04-09 16:38:02 +0200

    Fix jobs

--- tiago_pro_description/launch/robot_state_publisher.launch.py
+++ tiago_pro_description/launch/robot_state_publisher.launch.py
@@ -23,7 +23,6 @@ from launch import LaunchDescription
 from launch.substitutions import LaunchConfiguration
 from launch.actions import DeclareLaunchArgument, OpaqueFunction, SetLaunchConfiguration
 from launch_ros.actions import Node
-from launch_ros.parameter_descriptions import ParameterValue
 from launch_param_builder import load_xacro
 
 from launch_pal.arg_utils import LaunchArgumentsBase, read_launch_argument
